72d39744240c78ee47f2613390c87d678f02b131
webstartupaus <webstartupau@outlook.com>
2023-10-31 22:04:12 +1100

    Heroku update

--- app.mjs
+++ app.mjs
@@ -28,6 +28,8 @@ app.get('/', async (req, res) => {
     }
 });
 
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
