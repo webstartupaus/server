6375c1640111d1b763c27d4cc9b1a5581b26cbc8
webstartupaus <webstartupau@outlook.com>
2023-11-01 10:29:56 +1100

    T

--- app.mjs
+++ app.mjs
@@ -1,24 +1,22 @@
 import express, { response } from "express";
 import bodyParser from 'body-parser';
 import axios from "axios";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import { MongoClient } from 'mongodb';
-dotenv.config();
+// dotenv.config();
 
 const app = express();
 
 const myURL = new URL(process.env.FIXIE_URL);
 
-if (!process.env.PORT) {
-    axios.get('https://thewinklers-3cdfbdc10525.herokuapp.com', {
-        proxy: {
-            protocol: 'http',
-            host: myURL.hostname,
-            port: myURL.port,
-            auth: {username: myURL.username, password: myURL.password}
-        }
-    }).then(response => console.log(response.status));
-}
+axios.get('https://thewinklers-3cdfbdc10525.herokuapp.com', {
+    proxy: {
+        protocol: 'http',
+        host: myURL.hostname,
+        port: myURL.port,
+        auth: {username: myURL.username, password: myURL.password}
+    }
+}).then(response => console.log(response.status));
 
 const client = new MongoClient(process.env.MONGO_URL);
 
@@ -35,20 +33,22 @@ app.use(
 );
 
 app.get('/', async (req, res) => {
-    await connect();
+    // await connect();
 
-    try {
-        const db = client.db('sample_guides');
-        const planets = await db.collection('planets').find({}).toArray();
-        console.log('MU: ', db);
-        console.log('MU: ', planets);
-        res.send('planets').status(200);
-        return;
-    }
-    catch(e) {
-        console.log('error: ', e);
-        res.send(e).status(500);
-    }
+    res.send('planets').status(200);
+
+    // try {
+    //     const db = client.db('sample_guides');
+    //     const planets = await db.collection('planets').find({}).toArray();
+    //     console.log('MU: ', db);
+    //     console.log('MU: ', planets);
+    //     res.send('planets').status(200);
+    //     return;
+    // }
+    // catch(e) {
+    //     console.log('error: ', e);
+    //     res.send(e).status(500);
+    // }
 });
 
 const PORT = process.env.PORT || 5001;
